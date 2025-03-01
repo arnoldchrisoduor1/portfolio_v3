f2300ed238ed7e7f88a38d4c45b9f2067f6b65c0
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-03-01 18:33:10 +0300

    version 1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-};
+  output: 'export',
+  // ... any other configurations you have
+}
 
-export default nextConfig;
+module.exports = nextConfig
